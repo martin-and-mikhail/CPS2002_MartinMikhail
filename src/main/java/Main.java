c8369a91be0f5d67a249edc26156fe1449385052
martinbartolo <34104946+martinbartolo@users.noreply.github.com>
2019-04-04 19:33:15 +0200

    Jenkins Building Test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,7 @@ public class Main {
         System.out.println("Hello World!");
 
         System.out.println("This is a test by Mikhail");
+
+        System.out.println("Test by Martin to check Jenkins Building");
     }
 }
