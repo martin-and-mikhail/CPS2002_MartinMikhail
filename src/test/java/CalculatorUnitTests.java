05d348cebe612460ca3c8e6644ad35bdb9809ec4
martinbartolo <34104946+martinbartolo@users.noreply.github.com>
2019-04-09 14:36:21 +0200

    Added Calculator Subtraction With Tests

--- src/test/java/CalculatorUnitTests.java
+++ src/test/java/CalculatorUnitTests.java
@@ -3,7 +3,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CalculatorTest {
+public class CalculatorUnitTests {
     private Calculator calculator;
 
     @Before
@@ -16,6 +16,7 @@ public class CalculatorTest {
         calculator = null;
     }
 
+    // Addition Tests
     @Test
     public void TestAdd_testTwoPositiveNumbers_shouldBeAdding(){
         Assert.assertEquals(5, calculator.add(2,3));
@@ -44,5 +45,36 @@ public class CalculatorTest {
     @Test
     public void TestAdd_TwoZeros_shouldBeAdding(){
         Assert.assertEquals(0, calculator.add(0,0));
+    }
+
+    // Subtraction Tests
+    @Test
+    public void TestSubtract_testTwoPositiveNumbers_shouldBeSubtracting(){
+        Assert.assertEquals(-1, calculator.subtract(2,3));
+    }
+
+    @Test
+    public void TestSubtract_testPositiveAndNegative_shouldBeSubtracting(){
+        Assert.assertEquals(5, calculator.subtract(2,-3));
+    }
+
+    @Test
+    public void TestSubtract_testTwoNegativeNumbers_shouldBeSubtracting(){
+        Assert.assertEquals(1, calculator.subtract(-2,-3));
+    }
+
+    @Test
+    public void TestSubtract_testPositiveAndZero_shouldBeSubtracting(){
+        Assert.assertEquals(2, calculator.subtract(2,0));
+    }
+
+    @Test
+    public void TestSubtract_testNegativeAndZero_shouldBeSubtracting(){
+        Assert.assertEquals(-2, calculator.subtract(-2,0));
+    }
+
+    @Test
+    public void TestSubtract_TwoZeros_shouldBeSubtracting(){
+        Assert.assertEquals(0, calculator.subtract(0,0));
     }
 }
