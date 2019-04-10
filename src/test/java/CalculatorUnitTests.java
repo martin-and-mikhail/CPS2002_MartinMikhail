1c98a39105b09978bb35b49e9f52d54defb3a99d
martinbartolo <34104946+martinbartolo@users.noreply.github.com>
2019-04-10 15:46:12 +0200

    Fixed tests

--- src/test/java/CalculatorUnitTests.java
+++ src/test/java/CalculatorUnitTests.java
@@ -19,88 +19,105 @@ public class CalculatorUnitTests {
     // Addition Tests
     @Test
     public void TestAdd_testTwoPositiveNumbers_shouldBeAdding(){
-        Assert.assertEquals(5, calculator.add(2,3));
+        int a = 2, b = 3;
+        Assert.assertEquals(2 + 3, calculator.add(a,b));
     }
 
     @Test
     public void TestAdd_testPositiveAndNegative_shouldBeAdding(){
-        Assert.assertEquals(-1, calculator.add(2,-3));
+        int a = 2, b = -3;
+        Assert.assertEquals(2 + (-3), calculator.add(a,b));
     }
 
     @Test
     public void TestAdd_testTwoNegativeNumbers_shouldBeAdding(){
-        Assert.assertEquals(-5, calculator.add(-2,-3));
+        int a = -2, b = -3;
+        Assert.assertEquals((-2) + (-3), calculator.add(a,b));
     }
 
     @Test
     public void TestAdd_testPositiveAndZero_shouldBeAdding(){
-        Assert.assertEquals(2, calculator.add(2,0));
+        int a = 2, b = 0;
+        Assert.assertEquals(2 + 0, calculator.add(a,b));
     }
 
     @Test
     public void TestAdd_testNegativeAndZero_shouldBeAdding(){
-        Assert.assertEquals(-2, calculator.add(-2,0));
+        int a = -2, b = 0;
+        Assert.assertEquals((-2) + 0, calculator.add(a,b));
     }
 
     @Test
     public void TestAdd_TwoZeros_shouldBeAdding(){
-        Assert.assertEquals(0, calculator.add(0,0));
+        int a = 0, b = 0;
+        Assert.assertEquals(0 + 0, calculator.add(a,b));
     }
 
     // Subtraction Tests
     @Test
     public void TestSubtract_testTwoPositiveNumbers_shouldBeSubtracting(){
-        Assert.assertEquals(-1, calculator.subtract(2,3));
+        int a = 2, b = 3;
+        Assert.assertEquals(2 - 3, calculator.subtract(a,b));
     }
 
     @Test
     public void TestSubtract_testPositiveAndNegative_shouldBeSubtracting(){
-        Assert.assertEquals(5, calculator.subtract(2,-3));
+        int a = 2, b = -3;
+        Assert.assertEquals(2 - (-3), calculator.subtract(a,b));
     }
 
     @Test
     public void TestSubtract_testTwoNegativeNumbers_shouldBeSubtracting(){
-        Assert.assertEquals(1, calculator.subtract(-2,-3));
+        int a = -2, b = -3;
+        Assert.assertEquals((-2) - (-3), calculator.subtract(a,b));
     }
 
     @Test
     public void TestSubtract_testPositiveAndZero_shouldBeSubtracting(){
-        Assert.assertEquals(2, calculator.subtract(2,0));
+        int a = 2, b = 0;
+        Assert.assertEquals(2 - 0, calculator.subtract(a,b));
     }
 
     @Test
     public void TestSubtract_testNegativeAndZero_shouldBeSubtracting(){
-        Assert.assertEquals(-2, calculator.subtract(-2,0));
+        int a = -2, b = 0;
+        Assert.assertEquals(-2 - 0, calculator.subtract(a,b));
     }
 
     @Test
     public void TestSubtract_TwoZeros_shouldBeSubtracting(){
-        Assert.assertEquals(0, calculator.subtract(0,0));
+        int a = 0, b = 0;
+        Assert.assertEquals(0 - 0, calculator.subtract(a,b));
     }
 
     // Multiplication Tests
     @Test
     public void TestMultiply_testTwoPositiveNumbers_shouldBeMultiplying(){
-        Assert.assertEquals(10, calculator.multiply(2,5));
+        int a = 2, b = 5;
+        Assert.assertEquals(2 * 5, calculator.multiply(a,b));
     }
 
     @Test
     public void TestMultiply_testTwoNegativeNumbers_shouldBeMultiplying(){
-        Assert.assertEquals(10, calculator.multiply(-2,-5));
+        int a = -2, b = -5;
+        Assert.assertEquals((-2) * (-5), calculator.multiply(a,b));
     }
 
     @Test
     public void TestMultiply_testNegativeAndPositiveNumber_shouldBeMultiplying(){
-        Assert.assertEquals(-10, calculator.multiply(-2,5));
+        int a = -2, b = 5;
+        Assert.assertEquals((-2) * 5, calculator.multiply(a,b));
     }
 
     @Test
     public void TestMultiply_multiplyWithZero_shouldBeMultiplying(){
-        Assert.assertEquals(0, calculator.multiply(0,5));
+        int a = 0, b = 5;
+        Assert.assertEquals(0*5, calculator.multiply(a,b));
     }
 
     @Test
     public void TestMultiply_testTwoZeroValues_shouldBeMultiplying(){
-        Assert.assertEquals(0, calculator.multiply(0,0));
+        int a = 0, b = 0;
+        Assert.assertEquals(0*0, calculator.multiply(a,b));
     }
 }
