b14227f97c373f8567d1b5a9eca3f9f11823779f
MikCas <mikhail.cassar@gmail.com>
2019-04-13 11:19:26 +0200

    Commit the map feature

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
-        System.out.println("This is a test by Mikhail");
+        Map m = new Map();
 
-        System.out.println("Test2 by Martin to check Jenkins Building");
-
-        System.out.println("Test3 by Mikhail to test for ");
+        m.generate();
 
     }
 }
